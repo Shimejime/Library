b766b873a623abe6b857e0cdf18106300bc0fe1d
Rashi22 <yoshi3njm@gmail.com>
2019-06-18 19:36:49 +0900

    fix

---
+++ Other/template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <utility>
+#include <cstring>
+#include <iomanip>
+#include <numeric>
+#include <cmath>
+#include <cassert>
+#include <complex>
+#define repi(i, x, n) for(int i = x; i < n; i++)
+#define rep(i, n) repi(i, 0, n)
+#define ing long long
+using namespace std;
+const int INF = 1<<30;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-8;
+signed main(void)
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    return 0;
+}
+
+
