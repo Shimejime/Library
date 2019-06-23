badf744444480d758db1be0dc59f530562fb8a59
Rashi22 <yoshi3njm@gmail.com>
2019-06-23 14:10:22 +0900

    add LCM

---
+++ Math/LCM.cpp
@@ -0,0 +1,12 @@
+template<typename T>
+T GCD(T a, T b)
+{
+    if(b == 0) return a;
+    return GCD(b, a % b);
+}
+template<typename T>
+T LCM(T a, T b)
+{
+    T g = GCD(a, b);
+    return a / g * b;
+}
