924a440da595f302bbee4fe37de6f34f05fb8ace
Rashi22 <yoshi3njm@gmail.com>
2019-06-30 02:06:59 +0900

    fix Combination

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,23 +1,4 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <functional>
-#include <vector>
-#include <utility>
-#include <cstring>
-#include <iomanip>
-#include <numeric>
-#include <cmath>
-#include <cassert>
-using namespace std;
-using ll = long long;
-const int INF = 1<<30;
-const int MOD = 1e9 + 7;
-
-// cf. https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
-
-template<typename T>
-struct Combination
+template<typename T> struct Combination
 {
     vector<T> fact, rfact, inv;
     Combination(int n) : fact(n + 1), rfact(n + 1), inv(n + 1)
@@ -39,16 +20,5 @@ struct Combination
         return fact[n] * (rfact[k] * rfact[n - k] % MOD) % MOD;
     }
 };
-
-signed main(void)
-{
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    Combination<long long> c(10);
-    cout << c.run(5, 2) << endl;
-    cout << c.run(5, 3) << endl;
-    cout << c.run(6, 3) << endl;
-    return 0;
-}
 
 
