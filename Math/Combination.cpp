a8b6f2e77dd2798f80fd3b8ab0491f71ee57eff6
Rashi22 <yoshi3njm@gmail.com>
2019-07-26 09:58:26 +0900

    fix Combination

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -13,21 +13,11 @@ template<typename T> struct Combination
             rfact[i] = rfact[i - 1] * inv[i] % MOD;
         }
     }
-    T P(int n, int r)
+    T C(int n, int k)
     {
-        if(r < 0 or n < r) return 0;
-        return fact[n] * rfact[n - r];
-    }
-    T C(int n, int r)
-    {
-        if(n < 0 or n < r) return 0;
-        return fact[n] * rfact[r] * rfact[n - r];
-    }
-    T H(int n, int r)
-    {
-        if(n < 0 or r < 0) return 0;
-        return (r == 0 ? 1 C(n + r - 1, r));
+        if(n < k) return 0;
+        if(n < 0 or k < 0) return 0;
+        return fact[n] * (rfact[k] * rfact[n - k] % MOD) % MOD;
     }
 };
-
 
