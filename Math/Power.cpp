69114d21b9d323f1d2a24d52ae9a0b1b1cc4cb90
Shimejime <yoshi3njm@gmail.com>
2020-02-23 12:26:12 +0900

    update

--- Math/Power.cpp
+++ Math/Power.cpp
@@ -1,13 +1,16 @@
-//x^n mod
-template<typename Type> Type power(Type x, Type n, Type mod)
+#include <iostream>
+template<typename T>
+T power(const T& a, const T& x) //a^x
 {
-    Type res = 1;
-    while(n > 0)
-    {
-        if(n & 1) (res *= x) %= mod;
-        (x *= x) %= mod;
-        n >>= 1;
-    }
+    if(x == 0) return 1;
+    T res = power(a, x>>1);
+    res *= res;
+    if(x & 1) res *= a;
     return res;
 }
 
+int main(void)
+{
+    std::cout << power(2, 10) << std::endl; // 1024
+    return 0;
+}
