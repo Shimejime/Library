23d3d7ea7ffa243ebf1c908e98dc8425d6552ee6
Shimejime <yoshi3njm@gmail.com>
2020-05-22 21:20:13 +0900

    update template.cpp

--- Others/template.cpp
+++ Others/template.cpp
@@ -3,29 +3,34 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
-#include <utility>
 #include <cstring>
 #include <iomanip>
 #include <numeric>
 #include <cmath>
 #include <cassert>
-#include <complex>
-#define repi(i, x, n) for(int i = x; i < n; i++)
-#define rep(i, n) repi(i, 0, n)
-#define ing long long
+#include <queue>
+#include <map>
+#include <set>
 using namespace std;
-using ll = long long;
-const int INF = 1<<30;
-const int MOD = (int)1e9 + 7;
-const int MAX_N = (int)1e5 + 5;
+using Int = int64_t;
+const Int INF = 1<<30;
+const Int MOD = (Int)1e9 + 7;
+const Int MAX_N = (Int)1e5 + 5;
 #define debug(x) cout << #x << ": " << x << endl
 template<typename T1, typename T2>
 ostream &operator<<(ostream &os, const pair<T1, T2>& p) { os << p.first << " " << p.second; return os; }
 template<typename T>
 ostream &operator<<(ostream &os, const vector<T> &v) { for(int i = 0; i < (int) v.size(); i++) os << v[i] << (i + 1 != v.size() ? " " : ""); return os; }
-signed main(void)
+
+void solve()
 {
-    cin.tie(0);
+
+}
+
+signed main()
+{
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
+    solve();
     return 0;
 }
